ef239d7be34687ed654f350c855a96a6a6056428
Tomasz Dziemidowicz <td@ampliapps.com>
2017-03-15 12:56:01 +0100

    .NET c# v.3 sample

---
+++ DotNET C#/SQLiteSyncCOMDotNetClient/SQLiteSyncCOMClient.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using AuroraSyncService;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Linq;
+
+namespace SQLiteSyncCOMCsharp
+{
+    public class SQLiteSyncCOMClient
+    {
+        RestClient wsClient;
+        private string connString = null;
+
+        public SQLiteSyncCOMClient(string connectionString, string url)
+        {
+            this.connString = connectionString;
+            wsClient = new RestClient(url);
+        }
+
+        public void ReinitializeDatabase(string subscriberId)
+        {
+            //getting data from server
+            var request = new RestRequest("InitializeSubscriber/{subscriberUUID}", Method.GET);
+            request.AddUrlSegment("subscriberUUID", subscriberId);
+            request.AddHeader("Accept", "*/*");
+            IRestResponse response = wsClient.Execute(request);
+            Dictionary<string, string> dbSchema = JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Content);
+            
+            //we need to sort by key
+            var tmp = dbSchema.OrderBy(key => key.Key);
+            var dbSchemaSorted = tmp.ToDictionary((keyItem) => keyItem.Key, (valueItem) => valueItem.Value);
+
+            using (SQLiteConnection conn = new SQLiteConnection(this.connString))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand())
+                {
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    SQLiteHelper sh = new SQLiteHelper(cmd);
+
+                    sh.BeginTransaction();
+
+                    try
+                    {
+                        foreach (KeyValuePair<string, string> entry in dbSchemaSorted)
+                            if (!entry.Key.StartsWith("00000"))
+                            {
+                                sh.Execute(entry.Value);
+                            }
+                        sh.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        sh.Rollback();
+                        throw ex;
+                    }
+
+                    conn.Close();
+                }
+            }
+        }
+
+        private void SendChanges(string subscriberId)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(this.connString))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand())
+                {
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    SQLiteHelper sh = new SQLiteHelper(cmd);
+
+                    DataTable tables = sh.Select("select tbl_Name from sqlite_master where type='table' and sql like '%RowId%';");
+
+                    StringBuilder sqlitesync_SyncDataToSend = new StringBuilder();
+                    sqlitesync_SyncDataToSend.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?><SyncData xmlns=\"urn:sync-schema\">");
+
+                    foreach (DataRow table in tables.Rows)
+                    {
+                        string tableName = table["tbl_Name"].ToString();
+                        if (tableName.ToLower() != "MergeDelete".ToLower())
+                        {
+                            try
+                            {
+                                sqlitesync_SyncDataToSend.Append("<tab n=\"" + tableName + "\">");
+
+                                #region new records
+                                DataTable newRecords = sh.Select("select * from " + tableName + " where RowId is null;");
+                                sqlitesync_SyncDataToSend.Append("<ins>");
+                                foreach (DataRow record in newRecords.Rows)
+                                {
+                                    sqlitesync_SyncDataToSend.Append("<r>");
+                                    foreach (DataColumn column in newRecords.Columns)
+                                    {
+                                        if (column.ColumnName != "MergeUpdate")
+                                        {
+                                            sqlitesync_SyncDataToSend.Append("<" + column.ColumnName + ">");
+                                            sqlitesync_SyncDataToSend.Append("<![CDATA[" + record[column.ColumnName].ToString() + "]]>");
+                                            sqlitesync_SyncDataToSend.Append("</" + column.ColumnName + ">");
+                                        }
+                                    }
+                                    sqlitesync_SyncDataToSend.Append("</r>");
+                                }
+                                sqlitesync_SyncDataToSend.Append("</ins>");
+                                #endregion
+
+                                #region updated records
+                                DataTable updRecords = sh.Select("select * from " + tableName + " where MergeUpdate > 0 and RowId is not null;");
+                                sqlitesync_SyncDataToSend.Append("<upd>");
+                                foreach (DataRow record in updRecords.Rows)
+                                {
+                                    sqlitesync_SyncDataToSend.Append("<r>");
+                                    foreach (DataColumn column in updRecords.Columns)
+                                    {
+                                        if (column.ColumnName != "MergeUpdate")
+                                        {
+                                            sqlitesync_SyncDataToSend.Append("<" + column.ColumnName + ">");
+                                            if (record[column.ColumnName].GetType().Name == "Byte[]")
+                                                sqlitesync_SyncDataToSend.Append("<![CDATA[" + Convert.ToBase64String((byte[])record[column.ColumnName]) + "]]>");
+                                            else
+                                                sqlitesync_SyncDataToSend.Append("<![CDATA[" + record[column.ColumnName].ToString() + "]]>");
+                                            sqlitesync_SyncDataToSend.Append("</" + column.ColumnName + ">");
+                                        }
+                                    }
+                                    sqlitesync_SyncDataToSend.Append("</r>");
+                                }
+                                sqlitesync_SyncDataToSend.Append("</upd>");
+                                #endregion
+
+                                sqlitesync_SyncDataToSend.Append("</tab>");
+                            }
+                            catch (Exception ex)
+                            {
+                                throw ex;
+                            }
+                        }
+                    }
+
+                    #region deleted records
+                    DataTable delRecords = sh.Select("select * from MergeDelete;");
+                    sqlitesync_SyncDataToSend.Append("<delete>");
+                    foreach (DataRow record in delRecords.Rows)
+                    {
+                        sqlitesync_SyncDataToSend.Append("<r>");
+                        sqlitesync_SyncDataToSend.Append("<tb>" + record["TableId"].ToString() + "</tb>");
+                        sqlitesync_SyncDataToSend.Append("<id>" + record["RowId"].ToString() + "</id>");
+                        sqlitesync_SyncDataToSend.Append("</r>");
+                    }
+                    sqlitesync_SyncDataToSend.Append("</delete>");
+                    #endregion
+
+                    sqlitesync_SyncDataToSend.Append("</SyncData>");
+
+                    #region send changes to server
+                    JsonObject inputObject = new JsonObject();
+                    inputObject.Add("subscriber", subscriberId);
+                    inputObject.Add("content", sqlitesync_SyncDataToSend.ToString());
+                    inputObject.Add("version", "3");
+
+                    System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+                    byte[] bytes = encoding.GetBytes(inputObject.ToString());
+
+                    var request = new RestRequest("Send", Method.POST);
+                    request.AddHeader("Content-Type", "application/json");
+                    request.AddHeader("Accept", "*/*");
+                    request.AddHeader("charset", "utf-8");
+                    request.AddHeader("Content-Length", bytes.Length.ToString());
+
+                    request.AddParameter("application/json; charset=utf-8", inputObject.ToString(), ParameterType.RequestBody);
+                    request.RequestFormat = DataFormat.Json;
+              
+                    IRestResponse response = wsClient.Execute(request);
+                    #endregion
+
+                    #region clear update marker
+                    foreach (DataRow table in tables.Rows)
+                    {
+                        string tableName = table["tbl_Name"].ToString().ToLower();
+                        if (tableName != "MergeDelete".ToLower() && tableName != "MergeIdentity".ToLower())
+                        {
+                            string updTriggerSQL = (string)sh.ExecuteScalar("select sql from sqlite_master where type='trigger' and name like 'trMergeUpdate_" + tableName + "'");
+                            sh.Execute("drop trigger trMergeUpdate_" + tableName + ";");
+                            sh.Execute("update " + tableName + " set MergeUpdate=0 where MergeUpdate > 0;");
+                            sh.Execute(updTriggerSQL);
+                        }
+
+                        if (tableName == "MergeIdentity".ToLower())
+                            sh.Execute("update MergeIdentity set MergeUpdate=0 where MergeUpdate > 0;");
+                    }
+                    #endregion
+
+                    #region clear delete marker
+                    sh.Execute("delete from MergeDelete");
+                    #endregion
+
+                    conn.Close();
+                }
+            }
+        }
+
+        private void GetChangesFromServer(string subscriberId)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(this.connString))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand())
+                {
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    SQLiteHelper sh = new SQLiteHelper(cmd);
+
+                    DataTable tables = sh.Select("select tbl_Name from sqlite_master where type='table';");
+
+                    foreach (DataRow table in tables.Rows)
+                    {
+                        try
+                        {
+                            sh.BeginTransaction();
+
+                            var request = new RestRequest("Sync/{subscriberUUID}/{tableName}", Method.GET);
+                            request.AddUrlSegment("subscriberUUID", subscriberId);
+                            request.AddUrlSegment("tableName", table["tbl_Name"].ToString());
+                            request.AddHeader("Accept", "*/*");
+                            IRestResponse response = wsClient.Execute(request);
+                            List<DataObject> tablesData = JsonConvert.DeserializeObject<List<DataObject>>(response.Content);
+
+                            foreach (DataObject tableData in tablesData)
+                                if (tableData.SyncId > 0)
+                                {
+                                    sh.Execute(tableData.TriggerDeleteDrop);
+                                    sh.Execute(tableData.TriggerInsertDrop);
+                                    sh.Execute(tableData.TriggerUpdateDrop);
+
+                                    XmlDocument xmlRecords = new XmlDocument();
+                                    xmlRecords.LoadXml(tableData.Records);
+                                    XPathNavigator oRecordsPathNavigator = xmlRecords.CreateNavigator();
+                                    XPathNodeIterator oRecordsNodesIterator = oRecordsPathNavigator.Select("/records/r");
+
+                                    foreach (XPathNavigator oCurrentRecord in oRecordsNodesIterator)
+                                    {
+                                        string action = oCurrentRecord.GetAttribute("a", "");
+                                        XmlDocument xmlRecord = new XmlDocument();
+                                        xmlRecord.LoadXml("<?xml version=\"1.0\" encoding=\"utf-8\"?><columns>" + oCurrentRecord.InnerXml + "</columns>");
+                                        XPathNavigator oColumnsPathNavigator = xmlRecord.CreateNavigator();
+                                        XPathNodeIterator oColumnsNodesIterator = oColumnsPathNavigator.Select("/columns/c");
+                                        int coumnsCount = oColumnsNodesIterator.Count;
+
+                                        SQLiteParameter[] parameters = new SQLiteParameter[coumnsCount];
+                                        int idx = 0;
+                                        foreach (XPathNavigator oCurrentColumn in oColumnsNodesIterator)
+                                        {
+                                            SQLiteParameter parameter = new SQLiteParameter();
+                                            parameter.Value = oCurrentColumn.InnerXml;
+                                            parameters[idx] = parameter;
+                                            idx++;
+                                        }
+
+                                        switch (action)
+                                        {
+                                            case "1"://insert
+                                                sh.Execute(tableData.QueryInsert, parameters);
+                                                break;
+                                            case "2"://update
+                                                sh.Execute(tableData.QueryUpdate, parameters);
+                                                break;
+                                            case "3"://delete
+                                                sh.Execute(tableData.QueryDelete, parameters);
+                                                break;
+                                        }
+                                    }
+
+                                    sh.Execute(tableData.TriggerDelete);
+                                    sh.Execute(tableData.TriggerInsert);
+                                    sh.Execute(tableData.TriggerUpdate);
+
+                                    request = new RestRequest("CommitSync/{syncId}", Method.GET);
+                                    request.AddUrlSegment("syncId", tableData.SyncId.ToString());
+                                    request.AddHeader("Accept", "*/*");
+                                    IRestResponse responseCommit = wsClient.Execute(request);
+                                }
+
+                            sh.Commit();
+                      
+                        }
+                        catch (Exception ex)
+                        {
+                            sh.Rollback();
+                            throw ex;
+                        }
+                    }
+
+                    conn.Close();
+                }
+            }
+        }
+
+        public void SendAndRecieveChanges(string subscriberId)
+        {
+            SendChanges(subscriberId);
+            GetChangesFromServer(subscriberId);
+        }
+
+    }
+}
---
+++ DotNET C#/SampleApp/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reintialize database";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Send and Recieve";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(13, 43);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(714, 460);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(706, 434);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Actor";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(700, 428);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(706, 434);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Customer";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(700, 428);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(706, 434);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Film";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView3.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView3.MultiSelect = false;
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView3.Size = new System.Drawing.Size(706, 434);
+            this.dataGridView3.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Subscriber";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 515);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SQLite-sync.com sample .NET application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView3;
+    }
+}
+
