db74229dfef5fd44f90059022c34bb747cdef935
DESKTOP-PH52LF3\user <m.brzozowski@amplifier.com.pl>
2017-07-31 10:21:19 +0200

    Application name changed / external links app-in browser added

--- Ionic/SqliteSyncDemo/src/app/app.module.ts
+++ Ionic/SqliteSyncDemo/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { HttpModule } from '@angular/http';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { SQLite } from '@ionic-native/sqlite';
--- Ionic/SqliteSyncDemo/src/pages/home/home.ts
+++ Ionic/SqliteSyncDemo/src/pages/home/home.ts
@@ -38,5 +38,4 @@ export class HomePage {
     });
     actionSheet.present();
   }
-
 }
